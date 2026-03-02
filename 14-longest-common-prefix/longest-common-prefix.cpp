960912c157082388de4cefb54e14609a03286208
Vivekhash20 <en23cs3011141@medicaps.ac.in>
2026-03-02 12:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.83%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last = strs[strs.size()-1];
+        string ans="";
+
+        int minSize=min(first.size(),last.size());
+
+        for (int i=0;i<minSize;i++) {
+            if(first[i]!=last[i]) {
+                break;
+            }
+            ans+=first[i];
+        }
+        if (strs.empty()) {
+            return "";
+        }
+
+        return ans;
+        
+    }
+    
+};
